e734f20bde12276b202d7fcf0520c03c0c53317c
Camille Baillat <camille.baillat@protonmail.com>
2018-01-18 18:40:45 +0100

    [dev][libft] added an atoi64 bits and trying to get the stack to work

--- includes/common.h
+++ includes/common.h
@@ -1,20 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   checker.h                                          :+:      :+:    :+:   */
+/*   common.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cbaillat <cbaillat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/01/18 16:23:01 by cbaillat          #+#    #+#             */
-/*   Updated: 2018/01/18 17:37:51 by cbaillat         ###   ########.fr       */
+/*   Created: 2018/01/18 17:36:43 by cbaillat          #+#    #+#             */
+/*   Updated: 2018/01/18 18:06:57 by cbaillat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CHECKER_H
-# define CHECKER_H
+#ifndef COMMON_H
+# define COMMON_H
 
-# include <stdlib.h>
-# include "libft.h"
-# include "common.h"
+# define MALLOC_FAIL (int32_t) -2
+
+typedef struct	s_stack
+{
+	int32_t	top;
+	int32_t	*items;
+}				t_stack;
 
 #endif
--- srcs/checker/checker.c
+++ srcs/checker/checker.c
@@ -6,7 +6,7 @@
 /*   By: cbaillat <cbaillat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/18 16:15:49 by cbaillat          #+#    #+#             */
-/*   Updated: 2018/01/18 16:18:10 by cbaillat         ###   ########.fr       */
+/*   Updated: 2018/01/18 18:39:33 by cbaillat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,3 +17,39 @@
 */
 
 #include "checker.h"
+
+
+
+int	main (int ac, char **av)
+{
+	char	*line;
+	t_stack	*stack;
+	int32_t	gnl_status;
+	int32_t	checker_status;
+	int32_t	i;
+
+	if (ac < 2)
+		return (SUCCESS);
+	if ((stack = return_stack(ac - 1, &av[1])) == ERROR)
+		ft_putstr("Error\n");
+	i = 0;
+	while (i < stack->top)
+	{
+		ft_putstr("Stack #");
+		ft_putnbr(i);
+		ft_putstr(": ");
+		ft_putnbr(stack->items[i]);
+		ft_putstr("\n");
+	}
+	// while ((gnl_status = get_next_line(0, &line)) > FILE_READ)
+	// {
+		// do instruction;
+	// }
+	if (gnl_status == ERROR || checker_status == ERROR)
+		ft_putstr("Error\n");
+	else if (checker_status == SUCCESS)
+		ft_putstr("OK\n");
+	else if (checker_status == FAILURE)
+		ft_putstr("KO\n");
+	return (SUCCESS);
+}
